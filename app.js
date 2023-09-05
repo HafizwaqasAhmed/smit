ade8662dcde5bde621c7475dd5187c6e39df642b
Hafiz Waqas Ahmed <141481085+HafizwaqasAhmed@users.noreply.github.com>
2023-09-05 12:53:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// function abc(){ 
+//     var a="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo suscipit magnam aliquid nihil harum autem odio voluptates earum unde ad, quasi, animi voluptatum architecto ipsum id deleniti corporis, ex asperiores."
+//    document.getElementById("abc").innerhtml=a
+// }
+
+
+// let a =["milk" , "bread ", "apple"]
+
+// console.log(a)
+
+// a ={}
+
+// array = ["grapefruit", "orange", "lemon"];
+// array.push("tangerine");
+// console.log(array)
+// array.splice(2,0,"kela")
+// console.log(array)
+
+// let array1=["milk",2,apple]
+// array1.shift();
+// console.log(array)
+
+// let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+// console.log(names)
+// names.sort()
+// console.log(names)
+
+
+// let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+// console.log(names)
+// names.reverse()
+// console.log(names)
+
+let a = []
+a.push("milk","bread","apples")
+console.log(a)
+a.splice(1,1,"bananas","egg")
+console.log(a)
+a.pop()
+console.log(a)
+a.sort()
+console.log(a)
+a.splice(2,0,"carrots","lettuce")
+console.log(a)
+let b = ("juice","pop")
+let c = a.concat(b)
+console.log(c)
+c.sort()
+console.log(c)
